e2a4983593bc9fc531bdb196b7ab4b2b60532012
Santiago Soler <santiago.r.soler@gmail.com>
2019-02-25 17:11:27 -0300

    Improve aspect script

--- aspect.sh
+++ aspect.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-MODELPATH = "/home/dealii/aspect/model_input"
-IMAGE = "gassmoeller/aspect"
-TAG = "latest"
+GUEST_HOME=/home/dealii
+# Path to aspect binary inside the container
+ASPECT=$GUEST_HOME/aspect/aspect
+# Target where the host directory will be mounted
+MODELPATH=$GUEST_HOME/model
+# Docker image and tag
+IMAGE=gassmoeller/aspect
+TAG=latest
 
-# Pull ASPECT docker container
-echo "Pulling docker container..."
-docker pull $(IMAGE)
+# Get output directory from the .prm file
+OUTPUT_DIR=$(grep -i "set Output directory" $1 | awk '{printf $NF}')
 
-# Mount current directory into the docker container
-docker run -it -v "$(pwd):$(MODELPATH):ro" $(IMAGE):$(TAG)
-
-# Run the input file with ASPECT
-docker run -it "./aspect $(MODELPATH)/#1" $(IMAGE)
+# Run ASPECT
+docker run -it -v "$(pwd):$MODELPATH" $IMAGE:$TAG /bin/bash -c "$ASPECT $MODELPATH/$1; cp -r $OUTPUT_DIR $MODELPATH"
