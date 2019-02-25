d66197add5eccbe095c52c8adf4a29712309f8d1
Santiago Soler <santiago.r.soler@gmail.com>
2019-02-25 15:01:27 -0300

    Replace variable name for IMAGE

--- aspect.sh
+++ aspect.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 MODELPATH = "/home/dealii/aspect/model_input"
-ASPECT_DOCKER = "gassmoeller/aspect"
+IMAGE = "gassmoeller/aspect"
 TAG = "latest"
 
 # Pull ASPECT docker container
 echo "Pulling docker container..."
-docker pull $(ASPECT_DOCKER)
+docker pull $(IMAGE)
 
 # Mount current directory into the docker container
-docker run -it -v "$(pwd):$(MODELPATH):ro" $(ASPECT_DOCKER):$(TAG)
+docker run -it -v "$(pwd):$(MODELPATH):ro" $(IMAGE):$(TAG)
 
 # Run the input file with ASPECT
-docker run -it "./aspect $(MODELPATH)/#1" $(ASPECT_DOCKER)
+docker run -it "./aspect $(MODELPATH)/#1" $(IMAGE)
